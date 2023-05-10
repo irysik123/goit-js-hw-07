384f8be6c4cef6f78b3679e83c72c99131520ac9
irysik123 <vysotskaya.iryna@gmail.com>
2023-05-10 16:02:32 +0300

    new homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = (array) => 
+    array.map(element => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+          <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+          />
+        </a>
+      </li>`
+    }).join('');
+
+const galleryCards = markup(galleryItems);
+
+/* galleryEl.innerHTML = galleryCards;
+ */
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryCards);
+
+const onImgClick = event => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    } 
+    const instance = basicLightbox.create(`<img width="800" height="600" src="${event.target.dataset.source}">`);
+    instance.show();
+
+    const onEscClick = event => {
+        if(event.code !== 'Escape') {
+            return
+        }
+        instance.close();
+        window.document.removeEventListener('keydown', onEscClick)
+    }
+    window.document.addEventListener('keydown', onEscClick)
+}
+
+
+
+galleryEl.addEventListener('click', onImgClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = (array) => 
+    array.map(({ original, preview, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`
+    }).join('');
+
+const galleryCards = markup(galleryItems);
+
+/* galleryEl.innerHTML = galleryCards;
+ */
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryCards);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt",  captionDelay: 250 });
